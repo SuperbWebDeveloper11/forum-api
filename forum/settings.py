a2420617da8038bab416996b96a5fde783e96b3f
nabiliwas <nabiliwas360@gmail.com>
2020-09-25 21:23:06 +0100

    adding drf_yasg to INSTALLED_APPS

--- forum/settings.py
+++ forum/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'articles.apps.ArticlesConfig',
     'posts.apps.PostesConfig',
 ]
